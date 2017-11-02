ca55831bea7aa3066d09f73b038289f31de08a04
Joel Bennett <jaykul@huddledmasses.org>
2017-11-02 01:32:37 -0400

    Fix Test-Elevation to (maybe) work on posix

--- Source/Public/Test-Elevation.ps1
+++ Source/Public/Test-Elevation.ps1
@@ -1,10 +1,13 @@
 function Test-Elevation {
     <#
     .Synopsis
-        Get a value indicating whether the process is elevated (running as administrator)
+        Get a value indicating whether the process is elevated (running as administrator or root)
     #>
     [CmdletBinding()]
     param()
-
-    [Security.Principal.WindowsIdentity]::GetCurrent().Owner.IsWellKnown("BuiltInAdministratorsSid")
+    if(-not ($IsLinux -or $IsOSX)) {
+        [Security.Principal.WindowsIdentity]::GetCurrent().Owner.IsWellKnown("BuiltInAdministratorsSid")
+    } else {
+        0 -eq (id -u)
+    }
 }
