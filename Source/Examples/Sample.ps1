ba0081b86eee1b5ba925dc5dd19e853faae34103
Joel Bennett <Jaykul@HuddledMasses.org>
2018-10-25 22:12:10 -0400

    Update samples for v3

--- Source/Examples/Sample.ps1
+++ Source/Examples/Sample.ps1
@@ -1,27 +1,21 @@
-#requires -module @{ModuleName='PowerLine';ModuleVersion='1.1.0'}, @{ModuleName='PSGit'; ModuleVersion='2.0.3'}
-using module PowerLine
-using namespace PowerLine
+#requires -module @{ModuleName='PowerLine';ModuleVersion='3.0.5'}, @{ModuleName='PSGit'; ModuleVersion='2.0.4'}
 
+$global:prompt = @(
+    { "`t" } # On the first line, right-justify
+    { New-PowerLineBlock (Get-Elapsed) -ErrorBackgroundColor DarkRed -ErrorForegroundColor White -ForegroundColor Black -BackgroundColor DarkGray }
+    { Get-Date -f "T" }
+    { "`n" } # Start another line
+    { $MyInvocation.HistoryId }
+    { "&Gear;" * $NestedPromptLevel }
+    { if($pushd = (Get-Location -Stack).count) { "$([char]187)" + $pushd } }
+    { $pwd.Drive.Name }
+    { Split-Path $pwd -leaf }
+    # This requires my PoshCode/PSGit module and the use of the SamplePSGitConfiguration
+    { Get-GitStatusPowerline }
+)
 
-
-$global:PowerLinePrompt = 1,
-    # two lines
-    @(
-        # on the first line, two columns -- the first one is null (empty), the second is right-justified
-        $null,
-        @(
-            @{ text = { New-PowerLineBlock (Get-Elapsed) -ErrorBackgroundColor DarkRed -ErrorForegroundColor White -ForegroundColor Black -BackgroundColor DarkGray } }
-            @{ bg = "Gray";     fg = "Black"; text = { Get-Date -f "T" } }
-        )
-    ),
-    (
-        @{ bg = "Blue";     fg = "White"; text = { $MyInvocation.HistoryId } },
-        @{ bg = "Cyan";     fg = "White"; text = { [PowerLine.Prompt]::Gear * $NestedPromptLevel } },
-        @{ bg = "Cyan";     fg = "White"; text = { if($pushd = (Get-Location -Stack).count) { "$([char]187)" + $pushd } } },
-        @{ bg = "DarkBlue"; fg = "White"; text = { $pwd.Drive.Name } },
-        @{ bg = "DarkBlue"; fg = "White"; text = { Split-Path $pwd -leaf } },
-        # This requires my PoshCode/PSGit module and the use of the SamplePSGitConfiguration -- remove the last LeftCap in that.
-        @{ bg = "DarkCyan";               text = { Get-GitStatusPowerline } }
-    )
-
-Set-PowerLinePrompt -CurrentDirectory -PowerLineFont -Title { "PowerShell - {0} ({1})" -f (Convert-Path $pwd),  $pwd.Provider.Name }
+Set-PowerLinePrompt -SetCurrentDirectory -PowerLineFont -Title {
+    -join @(if (Test-Elevation) { "Administrator: " }
+        if ($IsCoreCLR) { "pwsh - " } else { "Windows PowerShell - "}
+        Convert-Path $pwd)
+} -Colors "White", "Gray", "Blue", "Cyan", "Cyan", "DarkBlue", "DarkBlue", "DarkCyan"
--- Source/Examples/SampleForConsolas.ps1
+++ Source/Examples/SampleForConsolas.ps1
@@ -1,28 +1,23 @@
-#requires -module @{ModuleName='PowerLine';ModuleVersion='1.1.0'}
-using module PowerLine
-using namespace PowerLine
+#requires -module @{ModuleName='PowerLine';ModuleVersion='3.0.5'}
 
-# Replace the Caps and Separators with characters that exist in Consolas and Courier New
-[PowerLine.Prompt]::ColorSeparator = [char]0x258C
-[PowerLine.Prompt]::ReverseColorSeparator = [char]0x2590
-[PowerLine.Prompt]::Separator = [char]0x25BA
-[PowerLine.Prompt]::ReverseSeparator = [char]0x25C4
+# You can change the separators by setting properties on [PoshCode.Pansies.Entities]::ExtendedCharacters
+# For example, this is an extra long backslash, in Consolas:
+[PoshCode.Pansies.Entities]::ExtendedCharacters.Separator = $([char]0x2572)
 
-$global:PowerLinePrompt = 1,
-    @(
-        # on the first line, two columns -- the first one is null (empty), the second is right-justified
-        $null,
-        @(
-            @{ bg = "DarkGray"; fg = "Black"; text = { Get-Elapsed } }
-            @{ bg = "Gray";     fg = "Black"; text = { Get-Date -f "T" } }
-        )
-    ),
-    @(
-        @{ bg = "Blue";     fg = "White"; text = { $MyInvocation.HistoryId } }
-        @{ bg = "Cyan";     fg = "White"; text = { [PowerLine.Prompt]::Gear * $NestedPromptLevel } }
-        @{ bg = "Cyan";     fg = "White"; text = { if($pushd = (Get-Location -Stack).count) { "$([char]187)" + $pushd } } }
-        @{ bg = "DarkBlue"; fg = "White"; text = { $pwd.Drive.Name } }
-        @{ bg = "DarkBlue"; fg = "White"; text = { Split-Path $pwd -leaf } }
-    )
+$global:Prompt = @(
+    { "`t" } # On the first line, right-justify
+    { Get-Elapsed }
+    { Get-Date -f "T" }
+    { "`n" } # Start another line
+    { $MyInvocation.HistoryId }
+    { "&Gear;" * $NestedPromptLevel }
+    { if($pushd = (Get-Location -Stack).count) { "$([char]187)" + $pushd } }
+    { $pwd.Drive.Name }
+    { Split-Path $pwd -leaf }
+)
 
-Set-PowerLinePrompt -ResetSeparators -CurrentDirectory -Title { "PowerShell - {0} ({1})" -f (Convert-Path $pwd),  $pwd.Provider.Name }
+Set-PowerLinePrompt -ResetSeparators -SetCurrentDirectory -Title {
+    -join @(if (Test-Elevation) { "Administrator: " }
+        if ($IsCoreCLR) { "pwsh - " } else { "Windows PowerShell - "}
+        Convert-Path $pwd)
+} -Colors DarkGray, Gray, Blue, Cyan, Cyan, DarkBlue, DarkBlue
