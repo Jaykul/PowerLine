d3bb1a24397ba5a3e318af56a49355441f1bb06f
Travis C. LaGrone <22419287+travis-c-lagrone@users.noreply.github.com>
2019-05-09 21:04:26 -0500

    Generalize Get-SegmentedPath to correctly handle other drive types

--- Source/Public/Get-SegmentedPath.ps1
+++ Source/Public/Get-SegmentedPath.ps1
@@ -41,49 +41,89 @@ function Get-SegmentedPath {
         [PoshCode.Pansies.RgbColor]$ErrorBackgroundColor
     )
 
-    $buffer = @()
+    # Define reusable constants.
+    $directorySeparatorChar = [System.IO.Path]::DirectorySeparatorChar
+    $ellipses = data { [char]0x2026 }
+    $resolvedHome = (Resolve-Path -LiteralPath $Home).Path
+    Write-Debug "`$directorySeparatorChar = '$directorySeparatorChar'"
+    Write-Debug "`$ellipses = '$ellipses'"
+    Write-Debug "`$resolvedHome = '$resolvedHome'"
 
-    if($Path.ToLower().StartsWith($Home.ToLower())) {
-        $Path = '~' + $Path.Substring($Home.Length)
+    # Resolve path.
+    $resolved = (Resolve-Path -LiteralPath $Path).Path
+    Write-Debug "`$resolved = '$resolved'"
+
+    # Split path into qualifier (or pseudo-qualifier) and no qualifier parts.
+    if($resolved.ToLower().StartsWith($resolvedHome.ToLower())) {
+        $qualifier = '~'
+        $noQualifier = $resolved.Substring($resolvedHome.Length)
+    } else {
+        $qualifier = Split-Path $resolved -Qualifier
+        $noQualifier = Split-Path $resolved -NoQualifier
     }
-    Write-Verbose $Path
-    while($Path) {
-        $buffer += if($Path -eq "~") {
-            @{ Object = $Path }
-        } else {
-            @{ Object = (Split-Path $Path -Leaf) -replace "[\\/]$" }
-        }
-        $Path = Split-Path $Path
+    Write-Debug "`$qualifier = '$qualifier'"
+    Write-Debug "`$noQualifier = '$noQualifier'"
 
-        Write-Verbose $Path
-
-        if($Path -and $SegmentLimit -le $buffer.Count) {
-            if($buffer.Count -gt 1) {
-                $buffer[-1] = @{ Object = [char]0x2026; }
-            } else {
-                $buffer += @{ Object = [char]0x2026; }
-            }
-            break
-        }
-
-        if($LengthLimit) {
-            $CurrentLength = ($buffer.Object | Measure-Object Length -Sum).Sum + $buffer.Count - 1
-            $Tail = if($Path) { 2 } else { 0 }
-
-            if($LengthLimit -lt $CurrentLength + $Tail) {
-                if($buffer.Count -gt 1) {
-                    $buffer[-1] = @{ Object = [char]0x2026; }
-                } else {
-                    $buffer += @{ Object = [char]0x2026; }
-                }
-                break
-            }
-        }
+    # Ensure that the path neither starts nor ends with the directory separator character. This is necessary in
+    # order to avoid a reundant empty leading and/or trailing path segment when splitting the path into segments.
+    $trimmed = $noQualifier
+    if($trimmed.StartsWith($directorySeparatorChar)) {
+        $trimmed = $trimmed.Substring(1)
     }
-    [Array]::Reverse($buffer)
+    if($trimmed.EndsWith($directorySeparatorChar)) {
+        $trimmed = $trimmed.Substring(0, $trimmed.Length - 1)
+    }
+    Write-Debug "`$trimmed = '$trimmed'"
 
-    foreach($output in $buffer) {
-        # Always set the defaults first, if they're provided
+    # Split path into segments.
+    $split = if($trimmed) { $trimmed.Split($directorySeparatorChar) } else { @() }
+    Write-Debug "`$split = @( $($split.ForEach{ "'$_'" } -join ', ') )"
+
+    #region Apply segment count and length limits.
+    $limited = @($qualifier) + $split
+    $prependEllipses = $false
+
+    function Test-SegmentLimit ([string[]] $Segments, [bool] $PrependEllipses) {
+        -not (
+            ($SegmentLimit -gt 0) -and  # There is a segment limit.
+            ($SegmentLimit -lt (  # The segment limit is exceeded.
+                $Segments.Count +
+                $(if ($PrependEllipses) { 1 } else { 0 })
+            ))
+        )
+    }
+    while (($limited.Count -gt 1) -and -not (Test-SegmentLimit $limited $prependEllipses)) {
+        $limited = $limited | Select-Object -Skip 1
+        $prependEllipses = $true
+    }
+
+    function Test-LengthLimit ([string[]] $Segments, [bool] $PrependEllipses) {
+        -not (
+            ($LengthLimit -gt 0) -and  # There is a length limit.
+            ($LengthLimit -lt (  # The length limit is exceeded.
+                (($Segments | Measure-Object 'Length' -Sum).Sum) +
+                ($Segments.Count - 1) +  # count of separators required (assuming separator length of one)
+                $(if ($PrependEllipses) { 2 } else { 0 })  # length of ellipses character and trailing separator
+            ))
+        )
+    }
+    while (($limited.Count -gt 1) -and -not (Test-LengthLimit $limited $prependEllipses)) {
+        $limited = $limited | Select-Object -Skip 1
+        $prependEllipses = $true
+    }
+
+    if ($prependEllipses) {
+        $limited = @($ellipses) + $limited
+    }
+
+    Write-Debug "`$limited = @( $( $limited.ForEach{ "'$_'" } -join ', ') )"
+    #endregion
+
+    # Prepare the path for conversion to Pansies text(s).
+    $buffer = $limited.ForEach{
+        $output = @{ Object = $_ }
+
+        # Always set the defaults first, if they're provided.
         if($PSBoundParameters.ContainsKey("ForegroundColor")) {
             $output.ForegroundColor = $ForegroundColor
         }
@@ -110,6 +150,11 @@ function Get-SegmentedPath {
                 $output.BackgroundColor = $ErrorBackgroundColor
             }
         }
+
+        $output
     }
+    Write-Debug "`$buffer = @( $($buffer.ForEach{ "@{ $(($_.GetEnumerator() | ForEach-Object { "$($_.Key) = '$($_.Value)'" }) -join '; ') }" } -join ', ') )"
+
+    # Output the segmented path as an array of Pansies texts.
     [PoshCode.Pansies.Text[]]$buffer
 }
