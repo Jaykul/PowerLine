5a99b31a3b6a1e1212a876ccb8cf82b0fc0f32a3
Joel Bennett <Jaykul@HuddledMasses.org>
2022-08-11 01:37:08 -0400

    Change escape sequences for WindowsPowerShell compatibility

--- Source/Examples/Darkly.ps1
+++ Source/Examples/Darkly.ps1
@@ -1,12 +1,12 @@
-#requires -Module PowerLine
+﻿#requires -Module Pansies, PowerLine
 [CmdletBinding()]param()
 
 Write-Verbose "First we set the default caps"
-[PoshCode.TerminalBlock]::DefaultCapsLeftAligned = "","`u{E0B0}"
-[PoshCode.TerminalBlock]::DefaultCapsRightAligned ="`u{E0B2}",""
+[PoshCode.TerminalBlock]::DefaultCapsLeftAligned = "","$([char]0xE0B0)"
+[PoshCode.TerminalBlock]::DefaultCapsRightAligned = "$([char]0xE0B2)",""
 
 Write-Verbose "Then we set the prompt"
-Set-PowerLinePrompt -SetCurrentDirectory -DefaultSeparator "`u{E0B1}","`u{E0B3}" -DefaultCapsLeftAligned "","`u{E0B0}" -DefaultCapsRightAligned "`u{E0B2}","" -Title {
+Set-PowerLinePrompt -SetCurrentDirectory -DefaultSeparator "$([char]0xE0B1)","$([char]0xE0B3)" -DefaultCapsLeftAligned "","$([char]0xE0B0)" -DefaultCapsRightAligned "$([char]0xE0B2)","" -Title {
     -join @(
         if (Test-Elevation) {
             "Administrator: "
@@ -19,9 +19,10 @@ Set-PowerLinePrompt -SetCurrentDirectory -DefaultSeparator "`u{E0B1}","`u{E0B3}"
         Convert-Path $pwd
     )
 } -Prompt @(
+    Show-HistoryId -Background 'SteelBlue1' -ErrorBackgroundColor '#8B2252'
     Show-Path -HomeString "&House;" -Separator '' -Background 'Gray100' -Foreground 'Black'
     Show-PoshGitStatus -Background 'Gray23'
     Show-Date -Format "h\:mm" -Prefix "🕒"  -Alignment 'Right' -Background 'Gray23'
     Show-ElapsedTime -Autoformat -Prefix "⏱️"  -Alignment 'Right' -Background 'Gray47'
-    Show-HistoryId -Background 'SteelBlue1' -ErrorBackgroundColor '#8B2252'
-) -Verbose # -LeftCap "","`u{E0B0}" -RightCap "`u{E0B2}",""
+    New-TerminalBlock -DFg 'White' -DBg '#63B8FF' -EFg 'White' -Cap '‍' -Content ''
+) -Verbose -PSReadLineContinuationPrompt '▌ ' -PSReadLineContinuationPromptColor '[38;2;99;184;255m' -PSReadLinePromptText '[48;2;99;184;255m[38;2;255;255;255m[49m[38;2;99;184;255m[0m','[48;2;139;34;82m[38;2;255;255;255m[49m[38;2;139;34;82m[0m'
--- Source/Private/_init.ps1
+++ Source/Private/_init.ps1
@@ -3,8 +3,8 @@ using namespace System.Management.Automation
 using namespace System.Collections.Generic
 using namespace PoshCode.Pansies
 
-[PoshCode.TerminalBlock]::DefaultCapsLeftAligned = "","`u{E0B0}"
-[PoshCode.TerminalBlock]::DefaultCapsRightAligned ="`u{E0B2}",""
+[PoshCode.TerminalBlock]::DefaultCapsLeftAligned = "","$([char]0xE0B0)"
+[PoshCode.TerminalBlock]::DefaultCapsRightAligned ="$([char]0xE0B2)",""
 
 # Ensure the global prompt variable exists and is typed the way we expect
 [System.Collections.Generic.List[PoshCode.TerminalBlock]]$Global:Prompt = [PoshCode.TerminalBlock[]]@(
