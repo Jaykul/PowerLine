d762e1b6d38cde260b6c68df716584d2a5cc6823
Joel Bennett <Jaykul@HuddledMasses.org>
2023-04-17 00:53:24 -0400

    Fix Remove-PowerLineBlock

--- Source/Public/Remove-PowerLineBlock.ps1
+++ Source/Public/Remove-PowerLineBlock.ps1
@@ -5,11 +5,15 @@ function Remove-PowerLineBlock {
         .Description
             This function exists primarily to ensure that modules are able to clean up the prompt easily when they're removed
         .Example
-            Remove-PowerLineBlock {
-                New-TerminalBlock { Show-ElapsedTime } -ForegroundColor White -BackgroundColor DarkBlue -ErrorBackground DarkRed -ElevatedForegroundColor Yellow
-            }
+            $Prompt[-1] | Remove
 
-            Removes the specified block. Note that it must be _exactly_ the same as when you added it.
+            Removes the last block from the prompt
+        .Example
+            Show-ElapsedTime | Add-PowerLineBlock
+
+            Remove-PowerLineBlock { Show-ElapsedTime }
+
+            Removes the Show-ElapsedTime block from the prompt
     #>
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidDefaultValueForMandatoryParameter', "Index",
         Justification = 'This rule should ignore parameters that are only mandatory in some parameter sets')]
@@ -24,30 +28,38 @@ function Remove-PowerLineBlock {
         [int]$Index = -1
     )
     process {
+        # We always remove by index so we can adjust the DefaultAddIndex
         if ($PSCmdlet.ParameterSetName -eq "ByObject") {
             if ($InputObject -is [PoshCode.TerminalBlock]) {
                 $Index = @($Global:Prompt).IndexOf($InputObject)
-            }
-            if ($Index -ge 0) {
-                $InputString = if ($InputObject.Object) {
-                    $InputObject.Object.ToString().Trim()
-                } else {
-                    $InputObject.ToString().Trim()
+                if ($Index -lt 0) {
+                    $InputObject = $InputObject.Content
                 }
-                $Index = @($Global:Prompt).ForEach{$_.Object.ToString().Trim()}.IndexOf($InputString)
             }
-        }
 
-        if ($Index -lt 0) {
+            if ($Index -lt 0) {
+                $Index = @($Global:Prompt).ForEach("Content").IndexOf($InputObject)
+            }
+
+            if ($Index -lt 0) {
+                $InputString = $InputObject.ToString().Trim()
+                $Index = @($Global:Prompt).ForEach{ $_.Content.ToString().Trim() }.IndexOf($InputString)
+                if ($Index -ge 0) {
+                    Write-Warning "Removing `$Prompt[$Index] from the prompt with partial match."
+                }
+            }
+        } elseif ($Index -lt 0) {
             $Index = $Global:Prompt.Count - $Index
         }
 
         if ($Index -ge 0) {
             $null = $Global:Prompt.RemoveAt($Index)
+            if ($Index -lt $Script:PowerLineConfig.DefaultAddIndex) {
+                $Script:PowerLineConfig.DefaultAddIndex--
+            }
+        } else {
+            Write-Error "Could not find $InputObject to remove from the prompt."
         }
 
-        if ($Index -lt $Script:PowerLineConfig.DefaultAddIndex) {
-            $Script:PowerLineConfig.DefaultAddIndex--
-        }
     }
 }
